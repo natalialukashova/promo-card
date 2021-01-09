1d83db1ba666cb56bbf7acfec5ed2d59c108b097
Natalia Lukashova <n.lukashowa@gmail.com>
2021-01-09 18:12:59 +0300

    dom

---
+++ index.js
@@ -0,0 +1,8 @@
+let charlistValue1 = document.querySelector('.card-description_charlist_value_1');
+charlistValue1.textContent = '4 Ядра';
+
+let charlistValue2 = document.querySelector('.card-description_charlist_value_2');
+charlistValue2.textContent = '6 ГБ';
+
+let charlistValue3 = document.querySelector('.card-description_charlist_value_3');
+charlistValue3.textContent = '100 Гб SSD';
